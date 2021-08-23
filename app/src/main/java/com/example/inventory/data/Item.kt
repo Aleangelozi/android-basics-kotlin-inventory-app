287d91f3cdf21a5874f858fa0e8e711e51f83b76
aleangelozi <aleangelozi@gmail.com>
2021-08-22 22:46:45 -0300

    Adding a RecyclerView

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -3,8 +3,12 @@ package com.example.inventory.data
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import java.text.NumberFormat
 
-@Entity(tableName = "item")
+/**
+ * Entity data class represents a single row in the database.
+ */
+@Entity
 data class Item(
     @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
@@ -13,6 +17,10 @@ data class Item(
     @ColumnInfo(name = "price")
     val itemPrice: Double,
     @ColumnInfo(name = "quantity")
-    val quantityInStock: Int
-) {
-}
+    val quantityInStock: Int,
+)
+/**
+ * Returns the passed in price in currency format.
+ */
+fun Item.getFormattedPrice(): String =
+    NumberFormat.getCurrencyInstance().format(itemPrice)
